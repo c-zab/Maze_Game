bbc97ad7ca5aefe54d5a85bb2c9330614cdf614a
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-03-08 14:01:07 -0500

    Adding Start scene

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -5,16 +5,17 @@
   let canvas = document.getElementById("canvas");
 
   let stage: createjs.Stage;
-  let StartLabel: objects.Label;
-  let startbutton: objects.Button;
 
   let assetsManager: createjs.LoadQueue;
   let assetsManifest: any[];
 
+  let currentScene: objects.Scene;
+  let currentState: number;
+
   assetsManifest = [{ id: "startButton", src: "/Assets/images/button.png" }];
 
   function Init(): void {
-    console.log("Initialization start");
+    console.log("Initialization start...");
     assetsManager = new createjs.LoadQueue();
     assetsManager.installPlugin(createjs.Sound);
     assetsManager.loadManifest(assetsManifest);
@@ -28,47 +29,37 @@
     stage.enableMouseOver(20);
     createjs.Ticker.framerate = 60;
     createjs.Ticker.on("tick", Update);
+
+    objects.Game.currentScene = config.Scene.START;
+    currentState = config.Scene.START;
+
     Main();
   }
 
   function Update(): void {
+    if (currentState != objects.Game.currentScene) {
+      Main();
+    }
+    currentScene.Update();
+
     stage.update();
   }
 
   function Main(): void {
-    console.log("Game Start...");
-
-    StartLabel = new objects.Label(
-      "Start",
-      "bold 60px",
-      "Arial",
-      "#ffffff",
-      320,
-      110,
-      true
-    );
-
-    stage.addChild(StartLabel);
-
-    startbutton = new objects.Button(assetsManager, "startButton", 320, 340);
-
-    startbutton.on("mousedown", mouseClickButton);
-
-    stage.addChild(startbutton);
-
-    tween();
-  }
-  // width = "640" 320 height = "480" 240
-  function tween() {
-    createjs.Tween.get(startbutton, { loop: -1 })
-      .to({ x: 320, y: 330 }, 500)
-      .to({ x: 320, y: 340 }, 500);
-  }
-
-  function mouseClickButton(): void {
-    StartLabel.text = "Clicked";
-    StartLabel.regX = StartLabel.getMeasuredWidth() * 0.5;
-    StartLabel.regY = StartLabel.getMeasuredHeight() * 0.5;
+    switch (objects.Game.currentScene) {
+      case config.Scene.START:
+        console.log("Game Start...");
+        stage.removeAllChildren();
+        currentScene = new scenes.StartScene(assetsManager);
+        stage.addChild(currentScene);
+        break;
+      case config.Scene.PLAY:
+        console.log("Let's Play...");
+        break;
+      case config.Scene.OVER:
+        console.log("Game over...");
+        break;
+    }
   }
 
   window.onload = Init;
