470382bea3b9751a7d1c2d301fee536df53ab641
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-03-08 11:03:33 -0500

    Added bouncing button

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -2,7 +2,8 @@
     // Global variables
     var canvas = document.getElementById("canvas");
     var stage;
-    var helloLabel;
+    var StartLabel;
+    var Startbutton;
     function Init() {
         console.log("Initialization start");
         Start();
@@ -10,6 +11,7 @@
     function Start() {
         console.log("Starting Application...");
         stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20);
         createjs.Ticker.framerate = 60;
         createjs.Ticker.on("tick", Update);
         Main();
@@ -19,8 +21,25 @@
     }
     function Main() {
         console.log("Game Start...");
-        helloLabel = new objects.Label("Start", "bold 60px", "Arial", "#ffffff", 320, 110, true);
-        stage.addChild(helloLabel);
+        StartLabel = new objects.Label("Start", "bold 60px", "Arial", "#ffffff", 320, 110, true);
+        stage.addChild(StartLabel);
+        Startbutton = new objects.Button("/Assets/images/button.png", 320, 340);
+        Startbutton.regX = Startbutton.getBounds().width * 0.5;
+        Startbutton.regY = Startbutton.getBounds().height * 0.5;
+        Startbutton.on("mousedown", mouseClickButton);
+        stage.addChild(Startbutton);
+        tween();
+    }
+    // width = "640" 320 height = "480" 240
+    function tween() {
+        createjs.Tween.get(Startbutton, { loop: -1 })
+            .to({ x: 320, y: 330 }, 500)
+            .to({ x: 320, y: 340 }, 500);
+    }
+    function mouseClickButton() {
+        StartLabel.text = "Clicked";
+        StartLabel.regX = StartLabel.getMeasuredWidth() * 0.5;
+        StartLabel.regY = StartLabel.getMeasuredHeight() * 0.5;
     }
     window.onload = Init;
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -3,7 +3,8 @@
   let canvas = document.getElementById("canvas");
 
   let stage: createjs.Stage;
-  let helloLabel: objects.Label;
+  let StartLabel: objects.Label;
+  let Startbutton: objects.Button;
 
   function Init(): void {
     console.log("Initialization start");
@@ -14,6 +15,7 @@
     console.log("Starting Application...");
 
     stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20);
     createjs.Ticker.framerate = 60;
     createjs.Ticker.on("tick", Update);
     Main();
@@ -26,7 +28,7 @@
   function Main(): void {
     console.log("Game Start...");
 
-    helloLabel = new objects.Label(
+    StartLabel = new objects.Label(
       "Start",
       "bold 60px",
       "Arial",
@@ -36,7 +38,29 @@
       true
     );
 
-    stage.addChild(helloLabel);
+    stage.addChild(StartLabel);
+
+    Startbutton = new objects.Button("/Assets/images/button.png", 320, 340);
+    Startbutton.regX = Startbutton.getBounds().width * 0.5;
+    Startbutton.regY = Startbutton.getBounds().height * 0.5;
+
+    Startbutton.on("mousedown", mouseClickButton);
+
+    stage.addChild(Startbutton);
+
+    tween();
+  }
+  // width = "640" 320 height = "480" 240
+  function tween() {
+    createjs.Tween.get(Startbutton, { loop: -1 })
+      .to({ x: 320, y: 330 }, 500)
+      .to({ x: 320, y: 340 }, 500);
+  }
+
+  function mouseClickButton(): void {
+    StartLabel.text = "Clicked";
+    StartLabel.regX = StartLabel.getMeasuredWidth() * 0.5;
+    StartLabel.regY = StartLabel.getMeasuredHeight() * 0.5;
   }
 
   window.onload = Init;
